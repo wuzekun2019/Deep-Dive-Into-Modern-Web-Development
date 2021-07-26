50e6ee2ea6162f9835724fa520de09818f0b5297
wuzekun2019 <wu.zekun@wustl.edu>
2021-07-25 20:18:49 -0400

    part2

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    const request= axios.get(baseUrl)
+    const nonExisting={
+        id:10000,
+        name:'This person is not saved to server',
+        number:'123456',
+    }
+    return request.then(response=>response.data)
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl,newObject)
+    return request.then(response=>response.data)
+}
+
+const update = (id,newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`,newObject)
+    return request.then(response=>response.data)
+}
+
+const deleteperson = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response=>response.data)
+}
+
+export default {getAll,create,update,deleteperson}
